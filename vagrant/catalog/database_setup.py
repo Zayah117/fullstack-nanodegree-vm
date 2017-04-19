3fc723ce00fd019801a275466138cd9c78038f2e
Zayah117 <isaiah.baker@simpledove.com>
2017-04-18 22:58:01 -0400

    Work on login

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,54 +1,60 @@
 import sys
-
 from sqlalchemy import Column, ForeignKey, Integer, String
-
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import relationship
-
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    email = Column(String(80), nullable=False)
+    picture = Column(String(250))
 
 class Restaurant(Base):
-	__tablename__ = 'restaurant'
+    __tablename__ = 'restaurant'
 
-	name = Column(String(80), nullable = False)
-	id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property
-	def serialize(self):
-		# Returns object data in easily serializeable format
-		return {
-			'id' : self.id,
-			'name' : self.name
-		}
+    @property
+    def serialize(self):
+        # Returns object data in easily serializeable format
+        return {
+            'id' : self.id,
+            'name' : self.name
+        }
 
 class MenuItem(Base):
-	__tablename__ = 'menu_item'
+    __tablename__ = 'menu_item'
 
-	name = Column(String(80), nullable = False)
-	id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
 
-	course = Column(String(250))
-	description = Column(String(250))
-	price = Column(String(8))
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
 
-	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-	restaurant = relationship(Restaurant)
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property
-	def serialize(self):
-		# Returns object data in easily serializeable format
-		return {
-			'name' : self.name,
-			'description' : self.description,
-			'id' : self.id,
-			'price' : self.price,
-			'course' : self.course,
-		}
+    @property
+    def serialize(self):
+        # Returns object data in easily serializeable format
+        return {
+            'name' : self.name,
+            'description' : self.description,
+            'id' : self.id,
+            'price' : self.price,
+            'course' : self.course,
+        }
 
 ### insert at end of file ###
 
